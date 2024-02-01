90273a2925bc2878522e3822bc07d5ddaaeccc40
rwbahe <rwbahe@gmail.com>
2024-02-01 08:54:09 -0700

    style update

--- scripts/ux.js
+++ scripts/ux.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     if (screen.width <= 962) {
         $("#ms_3-b-inn-dropdown").click(function() {
             $("#ms_3-b-inn-dropdown").css("opacity", "1");
-            $("#ms_3-b-inn-dropdown").css("max-height", "240px");
+            $("#ms_3-b-inn-dropdown").css("max-height", "280px");
         })
 
         $("#ms_3-b-resort-dropdown").click(function() {
